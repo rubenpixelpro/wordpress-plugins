72ee00ff94d7bf0a1523dd3283b9229b5043b8e2
rubenpixelpro <38692309+rubenpixelpro@users.noreply.github.com>
2019-08-14 13:03:44 +0200

    Video 3.1

---
+++ myplugin/myplugin.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: My Plugin
+Description: Bienvenidos a Pixelpro.
+Author:      Rubén Córcoles
+Version:     1.0
+License:     GPLv2 or later
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version
+2 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+with this program. If not, visit: https://www.gnu.org/licenses/
+*/
+
+
+if ( ! defined( 'ABSPATH' ) ) {
+
+	exit;
+
+}
