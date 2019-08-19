0d338eed170ecedf1a651c0a657a389311f5917b
rubenpixelpro <38692309+rubenpixelpro@users.noreply.github.com>
2019-08-19 22:37:47 +0200

    Video 3.5

--- myplugin/admin/settings-callbacks.php
+++ myplugin/admin/settings-callbacks.php
@@ -121,6 +121,73 @@ function myplugin_callback_field_select( $args ) {
 
 function myplugin_callback_validate_options( $input ) {
 
+	if ( isset( $input['custom_url'] ) ) {
+		
+		$input['custom_url'] = esc_url( $input['custom_url'] );
+		
+	}
+
+	if ( isset( $input['custom_title'] ) ) {
+		
+		$input['custom_title'] = sanitize_text_field( $input['custom_title'] );
+		
+	}
+
+	$radio_options = array(
+		'enable'  => 'Estilos activados',
+		'disable' => 'Estilos desactivados'
+	);
+	if ( ! isset( $input['custom_style'] ) ) {
+		
+		$input['custom_style'] = null;
+		
+	}
+	if ( ! array_key_exists( $input['custom_style'], $radio_options ) ) {
+		
+		$input['custom_style'] = null;
+		
+	}
+
+	if ( isset( $input['custom_message'] ) ) {
+		
+		$input['custom_message'] = wp_kses_post( $input['custom_message'] );
+		
+	}
+
+	if ( isset( $input['custom_footer'] ) ) {
+		
+		$input['custom_footer'] = sanitize_text_field( $input['custom_footer'] );
+		
+	}
+
+	if ( ! isset( $input['custom_toolbar'] ) ) {
+		
+		$input['custom_toolbar'] = null;
+		
+	}
+	$input['custom_toolbar'] = ($input['custom_toolbar'] == 1 ? 1 : 0);
+
+	$select_options = array(
+		'default'   => 'Default',
+		'light'     => 'Light',
+		'blue'      => 'Blue',
+		'coffee'    => 'Coffee',
+		'ectoplasm' => 'Ectoplasm',
+		'midnight'  => 'Midnight',
+		'ocean'     => 'Ocean',
+		'sunrise'   => 'Sunrise',
+	);
+	if ( ! isset( $input['custom_scheme'] ) ) {
+		
+		$input['custom_scheme'] = null;
+		
+	}
+	if ( ! array_key_exists( $input['custom_scheme'], $select_options ) ) {
+		
+		$input['custom_scheme'] = null;
+	
+	}
+
 	return $input;
 
 }
